cb77743070c8816cd302221131e7373112fadd5b
chrisdisneylandinfastructure <150170580+chrisdisneylandinfastructure@users.noreply.github.com>
2023-11-07 09:32:42 -0700

    Add files via upload

---
+++ ASUHelloWorldJavaFX.java
@@ -0,0 +1,33 @@
+package asuHelloWorldJavaFX;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class ASUHelloWorldJavaFX extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    public void start(Stage primaryStage) {
+    	System.out.println("Chris Pekin Hello World!");
+    	System.out.println("It started!");
+        primaryStage.setTitle("Chris Pekin Hello World");
+        Button btn = new Button();
+        btn.setText("Display: 'Chris Pekin says: Hello World!'");
+        btn.setOnAction(new EventHandler<>() {
+            public void handle(ActionEvent event) {
+                System.out.println("Chris Pekin: Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
